4c77b9f11cc8f2ec58d030fb9984753d9de90455
Phil Constantinou <constantinou@gmail.com>
2024-01-21 02:02:05 -0800

    See if the old indexs helped with race conditions

--- backends/postgres/migrations/20240101191302_add_unique_on_fingerprint_constraint.down.sql
+++ backends/postgres/migrations/20240101191302_add_unique_on_fingerprint_constraint.down.sql
@@ -1,3 +1,1 @@
-DROP CONSTRAINT neoq_jobs_fingerprint_constraint_idx;
-CREATE INDEX IF NOT EXISTS neoq_jobs_fingerprint_idx ON neoq_jobs (fingerprint, status);
-CREATE UNIQUE INDEX IF NOT EXISTS neoq_jobs_fingerprint_unique_idx ON neoq_jobs (queue, fingerprint, status) WHERE NOT (status = 'processed');
+DROP CONSTRAINT neoq_jobs_fingerprint_run_unique_idx;
--- backends/postgres/migrations/20240101191302_add_unique_on_fingerprint_constraint.up.sql
+++ backends/postgres/migrations/20240101191302_add_unique_on_fingerprint_constraint.up.sql
@@ -1,4 +1,1 @@
-DROP INDEX IF EXISTS neoq_jobs_fingerprint_idx;
-DROP INDEX IF EXISTS neoq_jobs_fingerprint_unique_idx;
-
-CREATE UNIQUE INDEX IF NOT EXISTS neoq_jobs_fingerprint_unique_idx ON neoq_jobs (queue, status, fingerprint, ran_at);
+CREATE UNIQUE INDEX IF NOT EXISTS neoq_jobs_fingerprint_run_unique_idx ON neoq_jobs (queue, status, fingerprint, ran_at);
